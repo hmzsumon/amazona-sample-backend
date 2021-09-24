e6ed8d2334837bb54321260e086bd0e09a106e78
Zaklaria <zsumonn@gmail.com>
2021-09-25 00:32:07 +0600

    added cors

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,13 @@ import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 import orderRouter from './routers/orderRouter.js';
 import uploadRouter from './routers/uploadRouter.js';
-
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // || 'mongodb://localhost/amazona'
 mongoose.connect(
